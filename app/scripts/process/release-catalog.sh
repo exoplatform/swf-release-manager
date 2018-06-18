88fbe14a57514aea2c10a6ea12842c66af7a86d9
Vincent Sellier <vsellier@exoplatform.com>
2018-06-18 14:41:07 +0200

    RELMAN-17 Allow to use credentials to access the catalog file

--- app/scripts/process/release-catalog.sh
+++ app/scripts/process/release-catalog.sh
@@ -11,14 +11,24 @@ set -o pipefail
 # Download a JSON file and save it as catalog.json.
 # The JSOn filename is based on the JIRA ID: <JIRA_ID>.json
 #
-function release_catalog_download_from_url {
-  printHeader "Download catalog from ${CATALOG_BASE_URL}/$1.json"
+function release_catalog_download_from_url() {
 
-  response=$(curl -sS -H "Content-Type: application/json" -v ${CATALOG_BASE_URL}/$1.json 2>/dev/null)
-  CATALOG=$(echo $response | json -g)
-  echo  $CATALOG > $DATAS_DIR/catalog.json
+	withCredentials=false
+	params=""
+	set +e
+	if [ ! -z "${CATALOG_CREDENTIALS}" ]; then
+		params="-u ${CATALOG_CREDENTIALS}"
+		withCredentials=true
+	fi
+	set -e
 
-  printFooter "Download catalog."
+	printHeader "Download catalog from ${CATALOG_BASE_URL}/$1.json withCredential=${withCredentials}"
+	response=$(curl -sS ${params} -H "Content-Type: application/json" -v ${CATALOG_BASE_URL}/$1.json 2>/dev/null)
+
+	CATALOG=$(echo $response | json -g)
+	echo $CATALOG >$DATAS_DIR/catalog.json
+
+	printFooter "Download catalog."
 }
 
 #
