c16d64f46ee864882e700ac76a52650fabb8c6aa
Houssem Ben Ali <hbenali@exoplatform.com>
2022-11-16 15:07:07 +0100

    Add Extra Maven opts to prevent overwrite default one

--- app/scripts/setup-env.sh
+++ app/scripts/setup-env.sh
@@ -65,6 +65,9 @@ replaceInFile $HOME/.m2/settings.xml @@GPG_KEY_NAME@@         $gpg_keyname
 replaceInFile $HOME/.m2/settings.xml @@TOOLS_DIR@@            $TOOLS_DIR
 replaceInFile $HOME/.m2/settings.xml @@SERVER_DIR@@           $LOCAL_DEPENDENCIES_DIR
 
+# Extra Maven OPTS
+export MAVEN_OPTS="${MAVEN_OPTS:-} ${MAVEN_EXTRA_OPTS:-}"
+
 # Install release.json file only if it doesn't already exist
 if [ -f "$WORKSPACE_DIR/release.json" ]
 then
