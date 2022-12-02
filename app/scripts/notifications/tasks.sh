2a9c4695e4b07db4f5d32ec5813a4155cdd5c6a1
Houssem Ben Ali <hbenali@exoplatform.com>
2022-12-02 10:12:04 +0100

    Re-enable Tribe task comments (#29)
    * Re-enable Tribe task comments
    
    * Exclude CI/CD task

--- app/scripts/notifications/tasks.sh
+++ app/scripts/notifications/tasks.sh
@@ -11,8 +11,10 @@ set -o pipefail
 function task_add_comment {
   project=$(getProjectByNameFromCatalog $1)
   status=$2
-  task_id=$3  
-  msg="@swf-release-manager (v ${EXOR_VERSION}) {{$project}}: $status"  
-  log "Add comment $msg to task $task_id";  
-  #curl -s -L -u $TRIBE_RELEASE_USER:$TRIBE_RELEASE_PASSWORD -XPOST -d " $msg " -v "$TRIBE_RELEASE_TASK_REST_PREFIXE_URL/$task_id" 2> /dev/null
+  task_id=$3
+  if [[ ! "${task_id}" =~ ^continuous-release-template ]]; then 
+    msg="@swf-release-manager (v ${EXOR_VERSION}) {{$project}}: $status"  
+    log "Add comment $msg to task $task_id";  
+    curl -s -L -u $TRIBE_RELEASE_USER:$TRIBE_RELEASE_PASSWORD -XPOST -d " $msg " -v "$TRIBE_RELEASE_TASK_REST_PREFIXE_URL/$task_id" 2> /dev/null
+  fi
 }
