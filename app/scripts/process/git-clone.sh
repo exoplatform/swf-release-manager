705ba29750c0b8d6b8d61c36ee24248132a88621
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-06-05 23:21:50 +0200

    Fix jq parsing on git clone single

--- app/scripts/process/git-clone.sh
+++ app/scripts/process/git-clone.sh
@@ -45,7 +45,7 @@ function git_clone_all_with_label {
 # Clone 1 project via its github name
 function git_clone_single {
   request="\"$1\""
-  ARR=($(jq -r '.[] | select(.name | contains('${request}')) | [.name, .git_organization, .release.version, .release.branch] | join(" ")' ${DATAS_DIR}/catalog.json))
+  ARR=($(jq -r '.[] | select(.name == '$request') | [.name, .git_organization, .release.version, .release.branch] | join(" ")' ${DATAS_DIR}/catalog.json))
   if [  -z ${ARR+x}  ]; then
     echo "No projects with name: " $1
   else
