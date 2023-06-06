516162416f0c0739935c0d3dec3d31c265149f43
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-06-06 08:23:37 +0200

    Ensure GPG compatibility with Ubuntu 20

--- app/conf/gpg-no-tty.sh
+++ app/conf/gpg-no-tty.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-/usr/bin/gpg --batch --no-tty --passphrase "@@GPG_KEY_PASSPHRASE@@" --trust-model always --yes "$@"
+OS_MAJOR_VERSION=$(/usr/bin/lsb_release -sr | cut -d '.' -f 1)
+if [ "${OS_MAJOR_VERSION}" -ge "18" ]; then 
+    /usr/bin/gpg --batch --pinentry-mode=loopback --no-tty --passphrase "@@GPG_KEY_PASSPHRASE@@" --trust-model always --yes "$@"
+else 
+    /usr/bin/gpg --batch --no-tty --passphrase "@@GPG_KEY_PASSPHRASE@@" --trust-model always --yes "$@"
+fi
