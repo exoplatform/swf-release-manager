45c073ebdbd6cc6b8ff6dd2762b5ed21434f586a
Houssem Ben Ali <hbenali@exoplatform.com>
2023-06-19 14:27:00 +0200

    GPG adapters: replace using lsb with gpg version check (#41)

--- app/conf/gpg-no-tty.sh
+++ app/conf/gpg-no-tty.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-OS_MAJOR_VERSION=$(/usr/bin/lsb_release -sr | cut -d '.' -f 1)
-if [ "${OS_MAJOR_VERSION}" -ge "18" ]; then 
+GPG_VERSION=$(/usr/bin/gpg --version 2>&1 | awk 'NR==1{print $NF}')
+if [ "${GPG_VERSION%%.*}" -ge "2" ]; then 
     /usr/bin/gpg --batch --pinentry-mode=loopback --no-tty --passphrase "@@GPG_KEY_PASSPHRASE@@" --trust-model always --yes "$@"
 else 
     /usr/bin/gpg --batch --no-tty --passphrase "@@GPG_KEY_PASSPHRASE@@" --trust-model always --yes "$@"
