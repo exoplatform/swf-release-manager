050bfe5f1fcdc842c3b4b50fdd5a701027ff775e
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-09-04 16:57:09 +0200

    Fix notification username variable typo

--- app/scripts/notifications/tasks.sh
+++ app/scripts/notifications/tasks.sh
@@ -13,7 +13,7 @@ function task_add_comment {
   status=$2
   task_id=$3
   if [[ ! "${task_id}" =~ ^continuous-release-template ]]; then 
-    msg="@${$TRIBE_RELEASE_USER} (v ${EXOR_VERSION}) <tt>$project</tt>: $status"
+    msg="@${TRIBE_RELEASE_USER} (v ${EXOR_VERSION}) <tt>$project</tt>: $status"
     log "Add comment $msg to task $task_id";
     # Run posting comment in subshell asynchronously  
     (curl -sSL -o /dev/null -u $TRIBE_RELEASE_USER:$TRIBE_RELEASE_PASSWORD -XPOST -H "Content-Type: application/json" -d "<p>$msg</p>" -v "$TRIBE_RELEASE_TASK_REST_PREFIXE_URL/$task_id" 2>/dev/null &)
