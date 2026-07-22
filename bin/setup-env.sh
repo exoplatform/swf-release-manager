11388819a6bf59b8c83ba103e39a1c9a3b8bc244
Houssem eXo <hbenali@exoplatform.fr>
2026-07-22 23:34:04 +0200

    chore: remove useless comment

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -95,5 +95,4 @@ ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 printFooter " ==> Credentials..."
 
 log "Execute eXo Release command...($*)"
-# Docker passes command as a single quoted argument; word-split for eXoR.sh
 exec "${SCRIPT_DIR}/eXoR.sh" $@
