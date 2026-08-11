2f224d0f5d94270b29cb7cc3221363aa2f3ce3d4
Houssem eXo <hbenali@exoplatform.fr>
2026-08-11 10:24:31 +0200

    Enhance gpg init script

--- app/conf/initgpg.sh
+++ app/conf/initgpg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-GPG_VERSION=$(/usr/bin/gpg --version 2>&1 | awk 'NR==1{print $NF}')
-if [ "${GPG_VERSION%%.*}" -ge "2" ]; then 
+GPG_MAJOR_VERSION=$(/usr/bin/gpg --version 2>/dev/null | sed -n '1s/.* \([0-9]\+\)\..*/\1/p')
+if [ "${GPG_MAJOR_VERSION}" -ge "2" ]; then
     /usr/bin/gpg --batch --pinentry-mode=loopback --no-tty --passphrase "@@GPG_KEY_PASSPHRASE@@" --trust-model always --yes --import $HOME/.gpg.key &>/dev/null
 else 
     /usr/bin/gpg --batch --no-tty --passphrase "@@GPG_KEY_PASSPHRASE@@" --trust-model always --yes --import $HOME/.gpg.key &>/dev/null
